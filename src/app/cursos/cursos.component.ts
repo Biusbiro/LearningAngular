19f809e334aef3cd7747ba518ed74e5816e0dc68
Bismarck Renan <bismarck@forlogic.net>
2019-10-02 15:50:08 -0300

    Adding Services and Dependency Injection

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,3 +1,4 @@
+import { CursosService } from './cursos.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,10 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
   nomeCurso: string;
-  cursos: string[] = ['Java', 'C#', 'Angular']
-
-  constructor() { 
+  cursos: string[];
+  
+  constructor(private cursosService: CursosService) { 
     this.nomeCurso = "Teste";
+    this.cursos = this.cursosService.getCursos();
   }
 
   ngOnInit() {
